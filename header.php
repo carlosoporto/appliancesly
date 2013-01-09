592dfee0ab936dcaa6014cd724704483daec66e4
Carlos Oporto <carlosoporto@gmail.com>
2013-01-08 22:56:46 -0500

    add "facebook header"

--- header.php
+++ header.php
@@ -125,4 +125,13 @@
 		
 		</header> <!-- end header -->
 		
+		<div id="fb-root"></div>
+		<script>(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = "//connect.facebook.net/es_LA/all.js#xfbml=1&appId=524504710906502";
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
+		
 		<div class="container-fluid">
