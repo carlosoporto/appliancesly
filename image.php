c00f4faf20635022736492019c7ca83a8df830c6
Carlos Oporto <carlosoporto@gmail.com>
2013-01-09 17:27:22 -0500

    add "addthis in image attachments"

--- image.php
+++ image.php
@@ -40,6 +40,18 @@ get_header(); ?>
 							      </a>
 							</div>
 							
+							<!-- AddThis Button BEGIN -->
+							<div class="addthis_toolbox addthis_default_style ">
+							<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
+							<a class="addthis_button_tweet"></a> 
+							<a class="addthis_button_linkedin_counter"></a>
+							<a class="addthis_button_google_plusone" g:plusone:size="medium" g:plusone:annotation="none"></a>
+							<a class="addthis_button_pinterest_pinit" pi:pinit:layout="horizontal"></a>
+							<a class="addthis_counter addthis_pill_style"></a>
+							</div>
+							<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-50ecf3ba5af64e38"></script>
+							<!-- AddThis Button END -->
+							
 							<!-- To display thumbnail of previous and next image in the photo gallery -->
 							<ul id="gallery-nav" class="clearfix">
 								<li class="next pull-left"><?php next_image_link() ?></li>
@@ -47,6 +59,16 @@ get_header(); ?>
 							</ul>
 							
 						</section> <!-- end article section -->
+						
+						<!-- AddThis Button BEGIN -->
+						<div class="addthis_toolbox addthis_floating_style addthis_counter_style" style="left:48px;top:180px;">
+						<a class="addthis_button_facebook_like" fb:like:layout="box_count"></a>
+						<a class="addthis_button_tweet" tw:count="vertical"></a>
+						<a class="addthis_button_google_plusone" g:plusone:size="tall"></a>
+						<a class="addthis_counter"></a>
+						</div>
+						<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-50ecf3ba5af64e38"></script>
+						<!-- AddThis Button END -->
 						
 						<footer>
 			
