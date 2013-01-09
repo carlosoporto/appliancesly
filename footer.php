64bbac72c4e5b77f30eeca8f877d6a6ab87c91d7
Carlos Oporto <carlosoporto@gmail.com>
2013-01-08 22:50:42 -0500

    "initial commit"

---
+++ footer.php
@@ -0,0 +1,37 @@
+			<footer role="contentinfo">
+			
+				<div id="inner-footer" class="clearfix">
+		          <hr />
+		          <div id="widget-footer" class="clearfix row-fluid">
+		            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer1') ) : ?>
+		            <?php endif; ?>
+		            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer2') ) : ?>
+		            <?php endif; ?>
+		            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer3') ) : ?>
+		            <?php endif; ?>
+		          </div>
+					
+					<nav class="clearfix">
+						<?php bones_footer_links(); // Adjust using Menus in Wordpress Admin ?>
+					</nav>
+					
+					<p class="pull-right">More in the network: <a href="http://jefefinal.com" id="JefeFinal" title="JefeFinal - Never Stop Playing">JefeFinal</a></p>
+			
+					<p class="attribution">Copyright &copy; 2013 Appliancesly · All rights reserved</p>
+				
+				</div> <!-- end #inner-footer -->
+				
+			</footer> <!-- end footer -->
+		
+		</div> <!-- end #container -->
+				
+		<!--[if lt IE 7 ]>
+  			<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+  			<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
+		<![endif]-->
+		
+		<?php wp_footer(); // js scripts are inserted using this function ?>
+
+	</body>
+
+</html>
