c7e395831d5b636aab74b35df50b6c948718948c
Carlos Oporto <carlosoporto@gmail.com>
2013-01-10 18:54:58 -0500

    change "index featured image"

--- index.php
+++ index.php
@@ -27,9 +27,9 @@
 						
 						<header>
 						
+						<div class="page-header"><h1 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1></div>
+						
 							<a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail( 'wpbs-featured' ); ?></a>
-							
-							<div class="page-header"><h1 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1></div>
 							
 							<p class="meta"><?php _e("Posted on", "bonestheme"); ?> <time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_date(); ?></time> <?php _e("by", "bonestheme"); ?> <?php the_author(); ?> <span class="amp"></span> <?php _e("in", "bonestheme"); ?> <?php the_category(', '); ?></p>
 						
